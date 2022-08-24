45d93f45ae1e0906454fc95b2a32150968b6a2c8
Uriel Emiliano <ugalu448@gmail.com>
2022-08-23 21:13:53 -0500

    Agregando metodo sumar complejos

--- src/Problemario/NumerosComplejos/p291.java
+++ src/Problemario/NumerosComplejos/p291.java
@@ -5,7 +5,8 @@ import java.util.StringTokenizer;
 
 public class p291 {
     public static void main(String[] args) {
-        
+        Utilities utilidades = new Utilities();
+        utilidades.procesoPrincipal();
     }
 }
 
@@ -20,6 +21,14 @@ class NumeroComplejo{
         return parteReal;
     }
 
+    public NumeroComplejo sumarNumerosComplejos(NumeroComplejo p_objNumeroComplejo) {
+        float resultadoParteReal = parteReal + p_objNumeroComplejo.getParteReal();
+        float resultadoParteImaginaria = parteImaginaria + p_objNumeroComplejo.getParteImaginaria();
+
+        NumeroComplejo objResultado = new NumeroComplejo(resultadoParteReal, resultadoParteImaginaria);
+        return objResultado;
+    }
+
     NumeroComplejo(float p_parteReal, float p_parteImaginaria){
         parteReal = p_parteReal;
         parteImaginaria = p_parteImaginaria;
@@ -28,28 +37,52 @@ class NumeroComplejo{
 
 class Utilities{
 
-    public NumeroComplejo procesoPrincipal() {
-
-        StringTokenizer stringTokenizer;
-        Scanner input = new Scanner(System.in);
+    public void procesoPrincipal() {
         String operador;
-        int contador;
+        Scanner input = new Scanner(System.in);
         float parteReal1, parteReal2, parteImaginaria1, parteImaginaria2;
+        NumeroComplejo objNumeroComplejo1, objNumeroComplejo2, objNumeroComplejoResultado;
 
-        String datosNumeros = input.next();
-        stringTokenizer = new StringTokenizer(datosNumeros);
+        //Entrada de datos y segmentacion de los numeros complejos con su operador
+        String datosNumeros = input.nextLine();
+        StringTokenizer sTokenizer = new StringTokenizer(datosNumeros);
 
-        for(contador = 1; contador <= datosNumeros.length();contador++){
-            parteReal1 = Float.parseFloat(stringTokenizer.nextToken());
-            parteImaginaria1 = Float.parseFloat(stringTokenizer.nextToken());
+        parteReal1 = Float.parseFloat(sTokenizer.nextToken());
+        parteImaginaria1 = obtenerValorParteImaginaria(sTokenizer.nextToken());
 
-            parteReal2 = Float.parseFloat(stringTokenizer.nextToken());
-            parteImaginaria2 = Float.parseFloat(stringTokenizer.nextToken());
+        parteReal2 = Float.parseFloat(sTokenizer.nextToken());
+        parteImaginaria2 = obtenerValorParteImaginaria(sTokenizer.nextToken());
 
-            operador = stringTokenizer.nextToken();
+        operador = sTokenizer.nextToken();
 
+        //Instacia de los numeros complejos
+        objNumeroComplejo1 = new NumeroComplejo(parteReal1, parteImaginaria1);
+        objNumeroComplejo2 = new NumeroComplejo(parteReal2, parteImaginaria2);
+
+        switch (operador) {
+            case "+":
+                objNumeroComplejoResultado = objNumeroComplejo1.sumarNumerosComplejos(objNumeroComplejo2);
+                break;
+        
+            default:
+                objNumeroComplejoResultado = new NumeroComplejo(0.0f, 0.0f);
+                break;
         }
 
+        System.out.println(objNumeroComplejoResultado.getParteReal() + " " + operador + objNumeroComplejoResultado.getParteImaginaria()+"i");
         input.close();
+   }
+
+   private float obtenerValorParteImaginaria(String parteImaginaria) {
+        String valorParteImaginaria = "";
+        char cifraParteImaginaria;
+        int index;
+
+        for(index = parteImaginaria.length()-2;index >= 0; index--){
+            cifraParteImaginaria = parteImaginaria.charAt(index);
+            valorParteImaginaria = cifraParteImaginaria + valorParteImaginaria;
+        }
+        
+        return (Float.parseFloat(valorParteImaginaria));
    }
 }
