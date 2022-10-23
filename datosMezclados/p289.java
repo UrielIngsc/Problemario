1026ee3a9cd12a46cccd67a29b862f4215351c5f
Uriel Galindo <ugalu448@gmail.com>
2022-10-23 12:22:04 -0500

    Segunda version p289

--- datosMezclados/p289.java
+++ datosMezclados/p289.java
@@ -7,149 +7,186 @@ import java.util.Stack;
 
 public class p289 {
     public static void main(String[] args) {
-        Utilidades utilidades = new Utilidades();
-        Scanner input = new Scanner(System.in);
-        String cadenaDatos;
-        int casosPrueba, casosResueltos;
+        try (Scanner input = new Scanner(System.in)) {
+            Utilidades utilidades = new Utilidades();
+            String cadenaDatos;
+            int casosPrueba, casosResueltos;
 
-        casosPrueba = Integer.parseInt(input.nextLine());
+            casosPrueba = Integer.parseInt(input.nextLine());
 
-        for(casosResueltos = 0; casosResueltos < casosPrueba; casosResueltos++){
-            try {
-                cadenaDatos = input.nextLine();
-                utilidades.realizarOperacionesMostrarResultados(cadenaDatos);
-                input.close();
-            } catch (Exception e) {
+            for(casosResueltos = 0; casosResueltos < casosPrueba; casosResueltos++){
                 
-            }
-        }  
-    }
-}
+                cadenaDatos = input.nextLine();
+                utilidades.realizarOperaciones(cadenaDatos); 
+            }  
 
-
-class Estructuras{
-    VerificadorOutput verificador = new VerificadorOutput();
-    private Stack<Integer> pila = new Stack<>();
-    private Queue<Integer> cola = new LinkedList<>();
-    private int elementosPila, elementosCola;
-    
-    public void input(int valorEntrada, boolean operacionParaPila){
-        if(operacionParaPila){
-            pila.push(valorEntrada);
-            elementosPila++;
-        }else{
-            cola.add(valorEntrada);
-            elementosCola++;
+            input.close();
+        } catch (NumberFormatException e) {
+            
         }
-    }
 
-    public String output(String cadenaValoresFinales, boolean operacionParaPila){
-        String underFlowPila = "##", underFlowCola = "$$";
-
-        if(operacionParaPila){
-            if(verificador.sePuedeHacerOutput(elementosPila)){
-                return cadenaValoresFinales += pila.pop();
-            }
-            else{
-                return cadenaValoresFinales += underFlowPila;
-            }
-        }
-        else{
-            if(verificador.sePuedeHacerOutput(elementosCola)){
-                return cadenaValoresFinales += cola.poll();
-            }
-            else{
-                return cadenaValoresFinales += underFlowCola;
-            }
-        }
-    }
-
-    public String liberarDatosEstructuras(){
-        String datosLiberados = "";
-        datosLiberados += hacerFree(false);
-
-        return datosLiberados += hacerFree(true);
-    }
-    private String hacerFree(boolean operacionParaPila){
-        String datosLiberados = "";
-
-        if(operacionParaPila){
-            while(elementosPila > 0){
-                datosLiberados += pila.pop();
-                elementosPila--;
-            }
-            return datosLiberados;
-        }
-        else{
-            while (elementosCola > 0) {
-                datosLiberados += pila.peek();
-                elementosCola--;
-            }
-            return datosLiberados;
-        }
     }
 }
 
 class Utilidades{
-    private Estructuras estructuras = new Estructuras();
+    private int datosRecorridos = 0;
+    private Pila pila = new Pila();
+    private Cola cola = new Cola();
+    private String cadenaDatosFinal = "";
 
-    public void realizarOperacionesMostrarResultados(String cadenaDatos){
-        String cadenaValoresFinales = "", operacionRealizar;
-        boolean esOperacionPila;
-        char arrayDatos[] = cadenaDatos.toCharArray();
-        int elementosRecorridos = 0, valorEntrada;
-
-        while (elementosRecorridos <= cadenaDatos.length()) {
-            operacionRealizar = obtenerValor(arrayDatos, elementosRecorridos);
-
-            if(operacionRealizar.equalsIgnoreCase("PU") || operacionRealizar.equalsIgnoreCase("LL")){
-                elementosRecorridos += 2;
-                valorEntrada = Integer.parseInt(obtenerValor(arrayDatos, elementosRecorridos));
-                esOperacionPila = determinarEstructuraOperacion(operacionRealizar);
-                estructuras.input(valorEntrada, esOperacionPila);
-                elementosRecorridos += 2;
-            }
-            else{
-                elementosRecorridos += 2;
-                esOperacionPila = determinarEstructuraOperacion(operacionRealizar);          
-                cadenaValoresFinales += estructuras.output(cadenaValoresFinales,esOperacionPila);
-            }
+    public void realizarOperaciones(String cadenaDatos){
+        char arrayCadenaDatos[] = cadenaDatos.toCharArray();
+        String infoObtenida;
+        
+        while (datosRecorridos < cadenaDatos.length()) {
+            infoObtenida = obtenerInfo(arrayCadenaDatos);
+            cadenaDatosFinal += evaluarOperacion(infoObtenida, arrayCadenaDatos);
         }
-        mostrarValores(cadenaValoresFinales, estructuras);
+
+        mostrarResultados();
     }
 
-    private String obtenerValor(char[] arrayDatos, int elementosRecorridos){
-        String operacion = "";
+    private String obtenerInfo(char[] arrayCadenaDatos){
+        String infoObtenida = "";
         int index;
-        for ( index = 0; index < 2; index++) {
-            operacion += arrayDatos[elementosRecorridos];
-            elementosRecorridos++;
-        }
 
-        return operacion;
+        for (index = 0;index < 2;index++) {
+            infoObtenida += arrayCadenaDatos[datosRecorridos];
+            datosRecorridos++;
+        }
+        return infoObtenida;
     }
-    private boolean determinarEstructuraOperacion(String operacion){
-        if(operacion.equalsIgnoreCase("PU") || operacion.equalsIgnoreCase("PO")){
-            return true;
+    
+    private String evaluarOperacion(String operacion, char[] arrayCadenaDatos){
+        if(operacion.equalsIgnoreCase("PU") || operacion.equalsIgnoreCase("LL")){
+            realizarInput(operacion, arrayCadenaDatos);
+            return "";
         }else{
-            return false;
+            return realizarOutput(operacion);
         }
     }
 
-    public void mostrarValores(String cadenaValoresFinales, Estructuras estructuras){
-        System.out.println(cadenaValoresFinales + estructuras.liberarDatosEstructuras());
+    private void realizarInput(String operacion, char[] arrayCadenaDatos){
+        int valorEntrada = Integer.parseInt(obtenerInfo(arrayCadenaDatos));
+
+        if(operacion.equalsIgnoreCase("PU")) pila.push(valorEntrada);
+        else cola.in(valorEntrada);
+    }
+
+    private String realizarOutput(String operacion){
+        if(operacion.equalsIgnoreCase("PO")){
+            return cadenaDatosFinal += pila.pop(cadenaDatosFinal);
+        }
+        else{
+            return cadenaDatosFinal += cola.out(cadenaDatosFinal);
+        }
+    }
+
+    private void mostrarResultados(){
+        liberarDatos();
+        System.out.println(cadenaDatosFinal);
+    }
+
+    private void liberarDatos(){
+        cadenaDatosFinal += cola.free();
+        cadenaDatosFinal += pila.free();
     }
 }
 
-class VerificadorOutput{
 
-    public boolean sePuedeHacerOutput(int elementosEstructura){
-        if(elementosEstructura > 0){
-            return true;
-        }else{
-            return false;
+class Pila extends UtilidadesEstructuras{
+    private Stack<Integer> pila = new Stack<>();
+    private int elementosPila = 0;
+    private String underFlow = "##";
+
+    public int getElementosPila() {
+        return elementosPila;
+    }
+    public Stack<Integer> getPila() {
+        return pila;
+    }
+
+    public void push(int valorEntrada){
+        pila.push(valorEntrada);
+        elementosPila++;
+    }
+    public String pop(String cadenaValoresFinales){
+        String elementoSalida;
+        if(verificarOutput(elementosPila)){
+            elementosPila--;
+            elementoSalida = String.valueOf(pila.pop());
+            return cadenaValoresFinales += elementoSalida;
+        }
+        else {
+            elementoSalida = underFlow;
+            return cadenaValoresFinales += elementoSalida;
+        }
+    }
+    public String free(){
+        String valoresLiberados = "", valorSalida;
+        if(verificarOutput(elementosPila)){
+            while(elementosPila > 0){
+                valorSalida = String.valueOf(pila.pop());
+                valoresLiberados += valorSalida;
+                elementosPila--;
+            }
+            return valoresLiberados;
+        }
+        else{
+            return valoresLiberados;
         }
     }
 }
 
+class Cola extends UtilidadesEstructuras{
+    private Queue<Integer> cola = new LinkedList<>();
+    private int elementosCola = 0;
+    private String underflow = "$$";
 
+    public Queue<Integer> getCola() {
+        return cola;
+    }
+    public int getElementosCola() {
+        return elementosCola;
+    }
+
+    public void in(int valorEntrada){
+        cola.add(valorEntrada);
+        elementosCola++;
+    }
+    public String out(String cadenaValoresFinales){
+        String valorSalida;
+        if(verificarOutput(elementosCola)){
+            elementosCola--;
+            valorSalida = String.valueOf(cola.remove());
+            return cadenaValoresFinales += valorSalida;
+        }else{
+            valorSalida = underflow;
+            return cadenaValoresFinales += underflow;
+        }
+    }
+    public String free(){
+        String valoresLiberados = "", valorSalida;
+        if(verificarOutput(elementosCola)){
+            while(elementosCola > 0){
+                valorSalida = String.valueOf(cola.remove());
+                valoresLiberados += valorSalida;
+                elementosCola--;
+            }
+            return valoresLiberados;
+        }
+        else{
+            return valoresLiberados;
+        }
+    }
+}
+class UtilidadesEstructuras{
+    protected boolean verificarOutput(int elementosTotales){
+        if(elementosTotales > 0 ) return true;
+        else return false;
+    }
+    // protected String aplicarFormato(String cadenaValoresFinales, String valorSalida){
+    //     if(cadenaValoresFinales.length() == 0){return valorSalida + ",";}
+    //     else{return "," + valorSalida;}
+    // }
+}
